ce68f4693b623481e906ca7fddc3e65f0361e2fa
ValeKI <demigliovalentina@gmail.com>
2018-10-11 19:39:16 +0200

    Una nuvola  in ASCII

---
+++ ValeKI/nuvola.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    std::cout<< "      __  _       \n"
+             << "    (    )  )     \n"
+             << "  (__.        )   \n"
+             << " (  _____ (      )\n\nQuesta dovrebbe essere una nuvola.\n";
+    return 0;
+}
